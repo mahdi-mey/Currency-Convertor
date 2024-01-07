cc0d0e96549c32490bd7c5938a70e95112c7d244
Mahdi <mahdimey17@gmail.com>
2024-01-07 19:33:43 +0330

    building simple ui almost finished

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello fucking world
+    <div className='app'>
+      <label>Ammount</label>
+      <input type="text" value={12000} />
+        <div className='selectBox-container'>
+          <div className='selectBox'>
+            <label>From</label>
+              <select>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="CAD">CAD</option>
+                <option value="INR">INR</option>
+              </select>
+          </div>
+          <div className='selectBox'>
+            <label>To</label>
+            <select>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="CAD">CAD</option>
+                <option value="INR">INR</option>
+              </select>
+          </div>
+        </div>
+      <p className='resault'>61300</p>
     </div>
   );
 }
