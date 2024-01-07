de53fc05d61bf45c65db53b0d71b423b20b0ca32
Mahdi <mahdimey17@gmail.com>
2024-01-07 23:12:52 +0330

    added state & controlled elements & fetched data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ammount, setAmmount] = useState(1)
+  const [fromCur, setFromCur] = useState('EUR')
+  const [toCur, setToCur] = useState('USD')
+
+  useEffect(function(){ 
+    async function fetchData(){
+      let data = await fetch('https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD') 
+      let res  = await data.json()
+      console.log(res);
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className='app'>
       <label>Ammount</label>
-      <input type="text" value={12000} />
+      <input type="text" value={ammount} onChange={(e) => setAmmount(Number(e.target.value))} />
         <div className='selectBox-container'>
           <div className='selectBox'>
             <label>From</label>
-              <select>
+              <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
@@ -17,7 +31,7 @@ function App() {
           </div>
           <div className='selectBox'>
             <label>To</label>
-            <select>
+            <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
